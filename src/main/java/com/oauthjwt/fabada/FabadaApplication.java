c0e85ffb8886d195ff0a425a15c310971e80599d
Fabiano Silva <FabianoSantos2004@outlook.com.br>
2025-12-17 11:00:57 -0300

    init project

---
+++ src/main/java/com/oauthjwt/fabada/FabadaApplication.java
@@ -0,0 +1,13 @@
+package com.oauthjwt.fabada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FabadaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FabadaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oauthjwt/fabada/FabadaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oauthjwt.fabada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FabadaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
